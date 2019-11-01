5d30ebb5af685f090aa885c9cfbd9401ec6662b6
siegrest <179900@ttu.ee>
2019-11-01 08:08:02 +0200

    Move enum var to function

--- src/app/pages/prices/prices-search/prices-search.component.ts
+++ src/app/pages/prices/prices-search/prices-search.component.ts
@@ -1,19 +1,12 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {InputType, SearchCriteria} from './search-option';
 
-/*
-
-TODO: This class is an absolute mess
-
-*/
-
 @Component({
   selector: 'app-prices-search',
   templateUrl: './prices-search.component.html',
   styleUrls: ['./prices-search.component.css']
 })
 export class PricesSearchComponent implements OnInit {
-  public inputType = InputType;
   @Input() searchCriteria: SearchCriteria[];
 
   constructor() {
@@ -25,5 +18,9 @@ export class PricesSearchComponent implements OnInit {
 
   getEnabledCriteria(): SearchCriteria[] {
     return this.searchCriteria.filter(c => c.enabled === true);
+  }
+
+  public getInputTypes() {
+    return InputType;
   }
 }
