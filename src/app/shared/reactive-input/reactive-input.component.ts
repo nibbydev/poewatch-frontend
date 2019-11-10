38f7a84fd20eeff3bc6ac142a3de139e94e64bb5
siegrest <179900@ttu.ee>
2019-11-10 09:54:56 +0200

    Add reactive input query param unset when default option selected

--- src/app/shared/reactive-input/reactive-input.component.ts
+++ src/app/shared/reactive-input/reactive-input.component.ts
@@ -3,6 +3,7 @@ import {SearchCriteria} from '../data/search-criteria';
 import {ActivatedRoute} from '@angular/router';
 import {PriceFilterService} from '../../services/price-filter.service';
 import {RouterHelperService} from '../../services/router-helper.service';
+import {first} from 'rxjs/operators';
 
 @Component({
   selector: 'app-reactive-input',
@@ -24,16 +25,38 @@ export class ReactiveInputComponent implements OnInit {
   }
 
   private onChange() {
-    let val = this.criteria.value;
-    if (val) {
-      val = val.toString().trim();
+    const queryParams = {};
+    queryParams[this.criteria.id] = this.criteria.value
+      ? this.criteria.value.trim()
+      : this.criteria.value;
+
+    // if it was empty string
+    if (!queryParams[this.criteria.id]) {
+      queryParams[this.criteria.id] = undefined;
     }
 
-    const queryParams = {};
-    queryParams[this.criteria.id] = val;
+    // if value is equal to default option, unset the query param
+    if (this.criteria.defaultOptionIndex !== null && this.criteria.options !== null) {
+      this.handleDefaultOption(queryParams);
+      return;
+    }
 
+    // todo: calls twice for league. bad for performance?
     this.routerHelperService.navigate(queryParams);
     this.filterService.sortEntries();
+  }
+
+  private handleDefaultOption(queryParams: {}): void {
+    this.criteria.options.pipe(first()).subscribe(o => {
+      const defaultOption = o[this.criteria.defaultOptionIndex];
+      if (defaultOption && queryParams[this.criteria.id] === defaultOption.value) {
+        queryParams[this.criteria.id] = undefined;
+      }
+
+      // todo: calls twice for league. bad for performance?
+      this.routerHelperService.navigate(queryParams);
+      this.filterService.sortEntries();
+    });
   }
 
 }
