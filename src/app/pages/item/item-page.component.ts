5290f6700d153dc5214ea2276fa8c8c04df8d22c
siegrest <179900@ttu.ee>
2019-11-13 08:29:07 +0200

    Add league selector

--- src/app/pages/item/item-page.component.ts
+++ src/app/pages/item/item-page.component.ts
@@ -3,6 +3,9 @@ import {ActivatedRoute} from '@angular/router';
 import {ItemService} from '../../services/item.service';
 import {ItemEntry} from '../../shared/data/item-entry';
 import {first} from 'rxjs/operators';
+import {SearchCriteria, SearchOption} from '../../shared/data/search-criteria';
+import {BehaviorSubject, Subject} from 'rxjs';
+import {GetEntry} from '../../shared/data/get-entry';
 
 @Component({
   selector: 'app-item-page',
@@ -12,6 +15,22 @@ import {first} from 'rxjs/operators';
 export class ItemPageComponent implements OnInit {
   public item: ItemEntry;
   private id: number | undefined;
+  private criteria: SearchCriteria = {
+    id: 'league',
+    title: null,
+    inputType: 'dropdown',
+    visible: false,
+    disabled: false,
+    value: null,
+    defaultOptionIndex: 0,
+    categories: null,
+    reset: false,
+    showSpinner: true,
+    options: new BehaviorSubject<SearchOption[]>(null),
+    showItem(e: GetEntry) {
+      return true;
+    }
+  };
 
   constructor(private activatedRoute: ActivatedRoute,
               private itemService: ItemService) {
@@ -19,12 +38,22 @@ export class ItemPageComponent implements OnInit {
 
   ngOnInit() {
     this.id = parseInt(this.activatedRoute.snapshot.queryParamMap.get('id'), 10);
-
     if (isNaN(this.id)) {
-      this.id = undefined;
-    } else {
-      this.itemService.makeRequest(this.id).pipe(first()).subscribe(i => this.item = i);
+      return;
     }
+
+    this.itemService.makeRequest(this.id).pipe(first()).subscribe(i => {
+      const options: SearchOption[] = i.leagues.map(l => {
+        const display = l.display ? (l.active ? l.display : '> ' + l.display) : l.name;
+
+        return {display, value: l.name};
+      });
+
+
+      (this.criteria.options as Subject<SearchOption[]>).next(options);
+      this.criteria.value = options[0].value;
+      this.item = i;
+    });
   }
 
 }
